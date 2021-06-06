ab9c943057c16359913e089d1396e70c8b85d556
lesson1 <abinin99@bk.ru>
2021-06-06 17:03:55 +0300

    add buttom

--- script.js
+++ script.js
@@ -1,91 +1,92 @@
 class GoodsItem {
-    constructor(title, price) {
-      this.title = title;
-      this.price = price;
-    }
-    render() {
-      return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
-    }
-}
-  
- 
-  
-class GoodsList {
-    constructor() {
-      this.goods = [];
-    }
-    fetchGoods() {
-      this.goods = [
-        { title: 'Shirt', price: 150 },
-        { title: 'Socks', price: 50 },
-        { title: 'Jacket', price: 350 },
-        { title: 'Shoes', price: 250 },
-      ];
-    }
-    render() {
-      let listHtml = '';
-      this.goods.forEach(good => {
-        const goodItem = new GoodsItem(good.title, good.price);
-        listHtml += goodItem.render();
-      });
-      document.querySelector('.goods-list').innerHTML = listHtml;
-    }
-    calcAllGoods() {
-        let totalPrice = 0;
-        this.goods.forEach(good => {
-            if(good.price !== undefined) {
-                totalPrice += good.price;
-                console.log(good.price);
-            }
-        });
-        let totalGoodsAnswer = "Все товары на сумму $" + totalPrice;
-        document.querySelector('.goods-total').innerHTML = totalGoodsAnswer;
-    }
+  constructor(title, price, button) {
+    this.title = title;
+    this.price = price;
+
   }
+  render() {
+    return `<div class="conteyner"><div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div><button class="button" ${onclick = new Basket("Привет").addToBasket()}> Добавить в корзину</button ></div > `;
+  }
+}
+
+
+
+class GoodsList {
+  constructor() {
+    this.goods = [];
+  }
+  fetchGoods() {
+    this.goods = [
+      { title: 'Shirt', price: 150 },
+      { title: 'Socks', price: 50 },
+      { title: 'Jacket', price: 350 },
+      { title: 'Shoes', price: 250 },
+    ];
+  }
+  render() {
+    let listHtml = '';
+    this.goods.forEach(good => {
+      const goodItem = new GoodsItem(good.title, good.price);
+      listHtml += goodItem.render();
+    });
+    document.querySelector('.goods-list').innerHTML = listHtml;
+  }
+  calcAllGoods() {
+    let totalPrice = 0;
+    this.goods.forEach(good => {
+      if (good.price !== undefined) {
+        totalPrice += good.price;
+        console.log(good.price);
+      }
+    });
+    let totalGoodsAnswer = "Все товары на сумму $" + totalPrice;
+    document.querySelector('.goods-total').innerHTML = totalGoodsAnswer;
+  }
+}
 
 
 
 
-  // Класс элемента корзины
+// Класс элемента корзины
 class BasketItem {
-    // По сути, нам нужно отображать в корзине те же самые элементы, что и в списке
-    constructor(title, price) {
-        this.title = title;
-        this.price = price;
-    }
+  // По сути, нам нужно отображать в корзине те же самые элементы, что и в списке
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
 }
 
-  // Класс корзины
+// Класс корзины
 class Basket {
-    constructor() {
-        // В классе корзины массив с добавленными товарами
-        this.addGoods = [];
+  constructor(button) {
+    // В классе корзины массив с добавленными товарами
+    this.addGoods = [];
+    this.button = button;
+  }
+  // Добавление товара в корзину (привязываем на нажатие кнопки)
+  addToBasket() { this.addGoods }
 
-    }
-    // Добавление товара в корзину (привязываем на нажатие кнопки)
-    addToBasket() {}
+  // Удаление товара из корзины (привязываем на нажатие кнопки)
+  deleteFromBasket() { }
 
-    // Удаление товара из корзины (привязываем на нажатие кнопки)
-    deleteFromBasket() {}
+  // Считаем стоимость и количество товаров в корзине
+  calcBasket() { }
 
-    // Считаем стоимость и количество товаров в корзине
-    calcBasket() {}
+  // Метод, который определяет, добавлены ли в корзину какие-либо товары и при их наличии активирует кнопку "Оформить заказ"
+  isOrder() { }
 
-    // Метод, который определяет, добавлены ли в корзину какие-либо товары и при их наличии активирует кнопку "Оформить заказ"
-    isOrder() {}
+  // Рендер динамического содержимого корзины
+  render() { }
 
-    // Рендер динамического содержимого корзины
-    render() {}
-
-    // Метод открывания корзины
-    openBasket() {}
+  // Метод открывания корзины
+  openBasket() { }
 }
-  
 
-  const list = new GoodsList();
+
+const list = new GoodsList();
 list.fetchGoods();
 
 window.onload = () => {
-    list.render();
-    
+  list.render();
+
 }
